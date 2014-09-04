5f57a98e6308cf3b8b5e2b77790df2ca9f552a4a
Sean Redmond <github-smr@sneakemail.com>
2014-09-04 17:00:47 -0400

    Add Hours and Times classes

--- lib/lionactor/location.rb
+++ lib/lionactor/location.rb
@@ -1,7 +1,20 @@
 module Lionactor
   class Location < Resource
+    def initialize(data, client)
+      super(data, client)
+      @hours = nil
+    end
+
     def phone
       @data['contacts']['phone']
+    end
+
+    def hours
+      if @hours.nil?
+        @hours = Lionactor::Hours.new(@data['hours'])
+      end
+
+      @hours
     end
 
   end
