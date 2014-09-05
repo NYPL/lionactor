3519b7f8acbc4b966879e8627fd6dcf498dbaa49
Sean Redmond <github-smr@sneakemail.com>
2014-09-05 11:32:23 -0400

    More tests, mostly for automatic methods

--- spec/location_spec.rb
+++ spec/location_spec.rb
@@ -7,16 +7,59 @@ describe Lionactor::Location do
     @loc = Lionactor::Location.new(data, client)
   end
 
-  it "has a name" do
-    expect(@loc.name).to eq "Mid-Manhattan Library"
-  end
+  describe "via automatic methods" do
+    it "has an id" do
+      expect(@loc.id).to eq "MML"
+    end
 
-  it "has an about statement" do
-    expect(@loc.about).to start_with "Established in 1970"
-  end
+    it "has a name" do
+      expect(@loc.name).to eq "Mid-Manhattan Library"
+    end
 
-  it "is fully accessible" do
-    expect(@loc.access).to eq "Fully Accessible"
+    it "has a slug" do
+      expect(@loc.slug).to eq "mid-manhattan-library"
+    end
+
+    it "has an about statement" do
+      expect(@loc.about).to start_with "Established in 1970"
+    end
+
+    it "is fully accessible" do
+      expect(@loc.access).to eq "Fully Accessible"
+    end
+
+    it "has a street address" do
+      expect(@loc.street_address).to eq "455 Fifth Avenue"
+    end
+
+    it "has a city" do
+      expect(@loc.locality).to eq "New York"
+    end
+
+    it "has a state" do
+      expect(@loc.region).to eq "NY"
+    end
+
+    describe "#postal code" do
+      # This is currently a mistake in the API
+      it "should be a string" do
+        pending "the API returning postal codes as strings"
+        expect(@loc.postal_code).to be_an_instance_of(String)
+      end
+
+      it "has a postal code" do
+        pending "the API returning postal codes as strings"
+        expect(@loc.postal_code).to eq "10016"
+      end
+    end
+
+    it "has a cross street" do
+      expect(@loc.cross_street).to eq "at 40th Street"
+    end
+
+    it "has a type" do
+      expect(@loc.type).to eq "circulating"
+    end      
   end
 end
  
