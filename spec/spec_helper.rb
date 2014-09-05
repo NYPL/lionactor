ac3f11e5748df2760372931576a24baab77e4fcd
Sean Redmond <github-smr@sneakemail.com>
2014-09-05 16:34:23 -0400

    Add Amenity class, tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,5 @@ cwd = File.dirname(__FILE__)
 
 MML = IO.read(File.join(cwd, "responses", "mml.json"))
 LOCATIONS = IO.read(File.join(cwd, "responses", "all-locations.json"))
+AMENITIES = IO.read(File.join(cwd, "responses", "all-amenities.json"))
+AMENITIES_MML = IO.read(File.join(cwd, "responses", "amenities-mml.json"))
