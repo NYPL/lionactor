9a13477a23ec8dac3e95bdcbe59fecc938bb204b
Sean Redmond <github-smr@sneakemail.com>
2014-09-27 19:37:47 -0400

    Test automatic methods

--- spec/amenity_spec.rb
+++ spec/amenity_spec.rb
@@ -1,13 +1,54 @@
 require "spec_helper"
+require "pp"
 
 describe Lionactor::Amenity do
   before :each do
-    data = JSON.parse(AMENITIES_MML)["amenities"].first['amenities'].first
+    data = JSON.parse(AMENITIES)["amenities"].first['amenities'].first
     @amenity = Lionactor::Amenity.new(data, nil)
   end
 
-  it "has a name" do
-    expect(@amenity.name).to eq "Computers for Public Use"
+  describe "via automatic methods" do
+    describe "#id" do
+      it "returns a string" do
+        pending "the API returning amenity ids as strings"
+        expect(@amenity.id).to be_an_instance_of String
+      end
+
+      it "is correct" do
+        pending "the API returning amenity ids as strings"
+        expect(@amenity.id).to eq "7950"
+      end
+    end
+
+    describe "#name" do
+      it "returns a string" do
+        expect(@amenity.name).to be_an_instance_of String
+      end
+
+      it "is correct" do
+        expect(@amenity.name).to eq "Computers for Public Use"
+      end
+    end
+
+    describe "#rank" do
+      it "returns an Integer" do
+        expect(@amenity.rank).to be_an_instance_of Fixnum
+      end
+
+      it "is correct" do
+        expect(@amenity.rank).to eq 1
+      end
+    end
+
+    describe "#category" do
+      it "returns a string" do
+        expect(@amenity.category).to be_an_instance_of String
+      end
+
+      it "is correct" do
+        expect(@amenity.category).to eq "Computer Services"
+      end
+    end
   end
 end
 
