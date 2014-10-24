93df63270c9d26eeda74b1d29a2633d2763445df
Sean Redmond <github-smr@sneakemail.com>
2014-10-24 09:19:03 -0400

    Add #action? method and tests

--- spec/amenity_spec.rb
+++ spec/amenity_spec.rb
@@ -2,69 +2,88 @@ require "spec_helper"
 require "pp"
 
 describe Lionactor::Amenity do
-  describe "via automatic methods" do
-    before :each do
-      data = JSON.parse(AMENITY)["amenity"]
-      @amenity = Lionactor::Amenity.new(data, nil)
-    end
-
-    describe "#id" do
-      it "returns a string" do
-        pending "the API returning amenity ids as strings"
-        expect(@amenity.id).to be_an_instance_of String
-      end
-
-      it "is correct" do
-        pending "the API returning amenity ids as strings"
-        expect(@amenity.id).to eq "7950"
-      end
-    end
-
-    describe "#name" do
-      it "returns a string" do
-        expect(@amenity.name).to be_an_instance_of String
-      end
-
-      it "is correct" do
-        expect(@amenity.name).to eq "Computers for Public Use"
-      end
-    end
-
-    describe "#rank" do
-      it "returns an Integer" do
-        expect(@amenity.rank).to be_an_instance_of Fixnum
-      end
-
-      it "is correct" do
-        expect(@amenity.rank).to eq 1
-      end
-    end
-
-    describe "#category" do
-      it "returns a string" do
-        expect(@amenity.category).to be_an_instance_of String
-      end
-
-      it "is correct" do
-        expect(@amenity.category).to eq "Computer Services"
-      end
-    end
-  end
-
   context "When loaded from single amenity endpoint" do
     before :each do
       data = JSON.parse(AMENITY)["amenity"]
       @amenity = Lionactor::Amenity.new(data, nil)
     end
 
+    describe "via automatic methods" do
+      before :each do
+        data = JSON.parse(AMENITY)["amenity"]
+        @amenity = Lionactor::Amenity.new(data, nil)
+      end
+
+      describe "#id" do
+        it "returns a string" do
+          pending "the API returning amenity ids as strings"
+          expect(@amenity.id).to be_an_instance_of String
+        end
+
+        it "is correct" do
+          pending "the API returning amenity ids as strings"
+          expect(@amenity.id).to eq "7950"
+        end
+      end
+
+      describe "#name" do
+        it "returns a string" do
+          expect(@amenity.name).to be_an_instance_of String
+        end
+
+        it "is correct" do
+          expect(@amenity.name).to eq "Computers for Public Use"
+        end
+      end
+      
+      describe "#rank" do
+        it "returns an Integer" do
+          expect(@amenity.rank).to be_an_instance_of Fixnum
+        end
+        
+        it "is correct" do
+          expect(@amenity.rank).to eq 1
+        end
+      end
+      
+      describe "#category" do
+        it "returns a string" do
+          expect(@amenity.category).to be_an_instance_of String
+        end
+        
+        it "is correct" do
+          expect(@amenity.category).to eq "Computer Services"
+        end
+      end
+    end
+
+    describe "#action?" do
+      it "has an action" do
+        expect(@amenity.action?).to eq true
+      end
+    end
+
     describe "#locations" do
       it "returns an array" do
         expect(@amenity.locations).to be_an_instance_of Array
       end
-
+      
       it "returns an array of Location objects" do
         expect(@amenity.locations.first)
           .to be_an_instance_of Lionactor::Location
+      end
+    end
+  end
+
+  context "With an amenity that has no action or info" do
+    before :each do
+      data = JSON.parse(AMENITY_NO_ACTION)["amenity"]
+      @amenity = Lionactor::Amenity.new(data, nil)
+    end
+
+    describe "#action?" do
+      it "does not have an action" do
+        expect(@amenity.action?).to eq false
       end
     end
   end
